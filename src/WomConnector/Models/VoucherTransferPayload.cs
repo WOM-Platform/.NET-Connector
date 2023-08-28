717fe9a87d0384653f540308473f7a5b840e7cd9
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2023-08-28 09:27:29 +0200

    Add voucher exchange payloads

---
+++ src/WomConnector/Models/VoucherTransferPayload.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+
+namespace WomPlatform.Connector.Models {
+    /// <summary>
+    /// Request payload for voucher transfer between users.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn)]
+    public class VoucherTransferPayload {
+
+        /// <summary>
+        /// Unique ID of the source.
+        /// </summary>
+        /// <remarks>
+        /// This must match the set source ID for voucher transfers between users. Other IDs will be rejected.
+        /// </remarks>
+        [JsonProperty("sourceId", Required = Required.Always)]
+        [JsonConverter(typeof(IdentifierConverter))]
+        public Identifier SourceId { get; set; }
+
+        /// <summary>
+        /// Nonce to prevent message repetition (base64-encoded).
+        /// </summary>
+        [JsonProperty("nonce", Required = Required.Always)]
+        public string Nonce { get; set; }
+
+        /// <summary>
+        /// Encrypted payload (instance of <see cref="Content" />).
+        /// </summary>
+        [JsonProperty("payload", Required = Required.Always)]
+        public string Payload { get; set; }
+
+        /// <summary>
+        /// Encrypted payload of <see cref="PaymentConfirmPayload"/>.
+        /// </summary>
+        [JsonObject(MemberSerialization.OptIn)]
+        public class Content {
+
+            [JsonProperty("password")]
+            public string Password { get; set; }
+
+            /// <summary>
+            /// Base64-encoded session key to be used in response.
+            /// </summary>
+            [JsonProperty("sessionKey", Required = Required.Always)]
+            public string SessionKey { get; set; }
+
+            [JsonProperty("vouchers", Required = Required.Always)]
+            public VoucherInfo[] Vouchers { get; set; }
+
+        }
+
+        /// <summary>
+        /// Single voucher identification data.
+        /// </summary>
+        [JsonObject(MemberSerialization.OptIn)]
+        public class VoucherInfo {
+
+            [JsonProperty("id", Required = Required.Always)]
+            [JsonConverter(typeof(IdentifierConverter))]
+            public Identifier Id { get; set; }
+
+            [JsonProperty("secret", Required = Required.Always)]
+            public string Secret { get; set; }
+
+        }
+
+    }
+}
---
+++ src/WomConnector/Models/VoucherTransferResponse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace WomPlatform.Connector.Models {
+
+    /// <summary>
+    /// Response payload for voucher transfer between users.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptIn)]
+    public class VoucherTransferResponse {
+
+        /// <summary>
+        /// Encrypted payload (instance of <see cref="Content" />).
+        /// </summary>
+        [JsonProperty("payload", Required = Required.Always)]
+        public string Payload { get; set; }
+
+        /// <summary>
+        /// Inner payload.
+        /// </summary>
+        [JsonObject(MemberSerialization.OptIn)]
+        public class Content {
+
+            /// <summary>
+            /// Identifies the registry.
+            /// </summary>
+            [JsonProperty("registryUrl", Required = Required.Always)]
+            public string RegistryUrl { get; set; }
+
+            /// <summary>
+            /// Returns the nonce originally sent by the source.
+            /// </summary>
+            [JsonProperty("nonce", Required = Required.Always)]
+            public string Nonce { get; set; }
+
+            /// <summary>
+            /// Voucher redemption OTC.
+            /// </summary>
+            [JsonProperty("otc", Required = Required.Always)]
+            public Guid Otc { get; set; }
+
+            /// <summary>
+            /// Voucher redemption password.
+            /// </summary>
+            [JsonProperty("password", Required = Required.Always)]
+            public string Password { get; set; }
+
+            /// <summary>
+            /// Voucher redemption link for clients.
+            /// </summary>
+            [JsonProperty("link", Required = Required.DisallowNull)]
+            public string Link { get; set; }
+
+            /// <summary>
+            /// Voucher count.
+            /// </summary>
+            [JsonProperty("count", Required = Required.Default)]
+            public int Count { get; set; }
+
+        }
+
+    }
+}
